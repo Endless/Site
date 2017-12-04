d8939fe22e3416a559fe58e82c44ae4867c6c817
Artu <artutogamer@gmail.com>
2017-12-03 21:38:57 -0600

    added fallback for no configured

--- inc/settings.php
+++ inc/settings.php
@@ -5,6 +5,23 @@ class Settings
     public function __construct()
     {
         $this->title = 'test';
+
+        $this->host = 'localhost';
+        $this->port = 3306;
+
+        $username = '';
+        $password = '';
+
+        if($username === "" || $password === "")
+        {
+            $this->redirect();
+        }
+    }
+
+    function redirect($url)
+    {
+        echo "<a href=\"$url\">Redirecting...</a>";
+        echo "<script data-cfasync=\"false\" type=\"text/javascript\">document.location=\"$url\";</script>";
     }
 }
 ?>
