e17bee65e7f84c76cd3b583fc29b43d9b3c5e7da
Artu <artutogamer@gmail.com>
2017-12-03 21:41:11 -0600

    die

---
+++ error/no_config.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Endless Web Dashboard - Unconfigured</title>
+    <link href="../inc/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="jumbotron">
+        <h2>Unconfigured</h2>
+
+        <div class="text-success">
+            Hi! Welcome to the Endless Web Dashboard!
+            <br>
+            To get started edit the file <a class="text-warning">inc/settings.php</a> and fill it with your database settings.
+            <br>
+            After doing that click <a class="text-warning">"Continue"</a>
+            <br>
+        </div>
+        <br>
+        <a href="../" class="btn btn-default">Continue</a>
+    </div>
+</div>
+</body>
+</html>
