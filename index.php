84dd0f8953d4575e71ab1c62edf29332aae21de9
Artu <artutogamer@gmail.com>
2017-12-05 20:35:02 -0600

    fixed typo

--- index.php
+++ index.php
@@ -19,11 +19,11 @@ $page = new Page();
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="#">Endless</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle" style="">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="colapse navbar-colapse" id="navbarColor01">
+    <div class="collapse navbar-collapse" id="navbarColor01">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
                 <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
