ea6c155f71e839d3fe152e0061b29650400b5a72
Artu <artutogamer@gmail.com>
2017-12-05 19:06:56 -0600

    change css

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $page = new Page();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <title>Endless - Home</title>
-    <link href="./inc/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://bootswatch.com/4/darkly/bootstrap.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrap.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
