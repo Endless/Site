84754a4e4b56252a3d4ece10362eda269b8fc708
Artu <artutogamer@gmail.com>
2017-12-05 18:21:18 -0600

    ok now this?

--- inc/header.php
+++ inc/header.php
@@ -14,29 +14,7 @@ class Header
     function printNavbar()
     {
         ?>
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Endless</a>
-        </div>
-        <div class="collapse-navbar-collapse" id="navbar">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li class="active"><a href="#">Dashboard</a></li>
-                <li class="active"><a href="#">GitHub</a></li>
-                <li class="active"><a href="#">Support</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
+
         <?php
     }
 }
--- index.php
+++ index.php
@@ -17,6 +17,29 @@ $page = new Page();
     <script src="https://maxcdn.bootstrap.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Endless</a>
+        </div>
+        <div class="collapse-navbar-collapse" id="navbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li class="active"><a href="#">Dashboard</a></li>
+                <li class="active"><a href="#">GitHub</a></li>
+                <li class="active"><a href="#">Support</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 <?php //$header->printNavbar();
 echo "die"; ?>
 <div class="container">
