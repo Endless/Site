11bf0ac944218d395a46e288a6403f4549438c3c
Artu <artutogamer@gmail.com>
2017-12-20 20:18:18 -0600

    some additions

--- api/discord.js
+++ api/discord.js
@@ -1,13 +1,51 @@
 const express = require('express');
+const fetch = require('node-fetch');
+const btoa = require('btoa');
 const config = require('../config.json');
 const router = express.Router();
+const { catchAsync } = require('../utils');
 
-const CLIENT_ID = config.id;
+const CLIENT_ID = 310578566695878658;
 const CLIENT_SECRET = config.secret;
 const REDIR = encodeURIComponent(config.redirect);
 
 router.get('/login', (req, res) => {
-    res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${REDIR}`);
+    res.redirect(`https://discordapp.com/oauth2/authorize?client_id=310578566695878658&scope=identify&response_type=code&redirect_uri=${REDIR}`);
 });
+
+router.get('/callback', catchAsync(async (req, res) => {
+    if(!req.query.code) throw new Error('NoCodeProvided');
+
+    const code = req.query.code;
+    const creds = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+    const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${REDIR}`,
+        {
+            method: 'POST',
+            headers: {
+                    Authorization: `Basic ${creds}`,
+                },
+        });
+    const json = await response.json();
+    res.cookie('token', `${json.access_token}`);
+    res.redirect('/api/discord/login/success');
+}));
+
+router.get('/login/success', catchAsync(async (req, res) => {
+
+    console.info(`Request: Cookies: ${req.cookies}`)
+    //if(!req.cookies) throw new Error('NoCookies');
+
+    /*const token = req.cookies.token;
+    const response = await fetch('https://discordapp.com/api/users/@me',
+        {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+        });
+    const json = response.json();
+
+    res.send(`Welcome, ${json.username}#${json.discriminator} (ID: ${json.id})`)*/
+}));
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const https = require('https');
+const cookies = require('cookie-parser');
 const path = require('path');
 const config = require('./config.json');
 const { readFileSync } = require('fs');
@@ -9,12 +10,31 @@ if(config.portHttp==="" || config.portHttps==="")
     throw new Error('Ports not defined!');
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, 'index.html'))
+    res.status(200).sendFile(path.join(__dirname, 'index.html'));
+    console.info(`Request: Cookies: ${req.cookies}`)
 });
 
 app.use('/api/discord', require('./api/discord'));
 
-app.listen(config.portHttps, () => {
+app.use((err, req, res, next) => {
+    switch(err.message)
+    {
+        case 'NoCodeProvided':
+            return res.status(400).send({
+                status: 'ERROR',
+                error: err.message,
+            });
+        default:
+            return res.status(500).send({
+                status: 'ERROR',
+                error: err.message,
+            });
+    }
+});
+
+app.use(cookies());
+
+app.listen(config.portHttp, () => {
 	console.info(`[Express HTTP] Running on port ${config.portHttp}`);
 });
 
@@ -22,5 +42,5 @@ app.listen(config.portHttps, () => {
     "key": readFileSync(''),
     "cert": readFileSync(''),
 }, app).listen(config.portHttps, () => {
-    console.log(`[Express HTTP] Running on port ${config.portHttps}`)
+    console.log(`[Express HTTPS] Running on port ${config.portHttps}`)
 });*/
